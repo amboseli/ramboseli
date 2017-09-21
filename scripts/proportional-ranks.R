a595c7f5093be0940afde254227ed171a1fc194d
Fernando Campos <camposfa@gmail.com>
2017-09-21 14:15:39 -0400

    Proportional ranks script

---
+++ scripts/proportional-ranks.R
@@ -0,0 +1,25 @@
+Sys.setenv(TZ = 'UTC')
+list.of.packages <- list("ramboseli", "tidyverse", "forcats")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if (length(new.packages)) install.packages(unlist(new.packages))
+lapply(list.of.packages, require, character.only = T)
+
+babase <- DBI::dbConnect(RPostgreSQL::PostgreSQL(),
+                         host = "localhost",
+                         port = 2222,
+                         user = "fac13",
+                         dbname = "babase")
+
+ranks <- collect(tbl(babase, "ranks"))
+rank_dates <- collect(tbl(babase, "rankdates"))
+
+ranks_w_dates <- ranks %>%
+  left_join(rank_dates, by = "sname")
+
+prop_ranks <- ranks_w_dates %>%
+  filter(!(rnktype == "ALM" & rnkdate < ranked)) %>%
+  group_by(grp, rnkdate, rnktype) %>%
+  mutate(prop_rank = 1 - (rank - 1) / (n() - 1))
+
+prop_ranks$rnktype <- forcats::fct_recode(prop_ranks$rnktype, ADM = "ALM")
+
